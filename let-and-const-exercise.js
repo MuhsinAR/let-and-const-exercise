07f17ba559d95de32740aa46739225dcc56e89bf
MuhsinAR <135045308+MuhsinAR@users.noreply.github.com>
2023-06-08 13:01:46 -0400

    Add files via upload

---
+++ let-and-const-exercise.js
@@ -0,0 +1,18 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+/* Write an ES2015 Version */
+
+let PI = 3.14;
+PI = 42;
+
+//- What is the difference between var and let?
+// Var lets you redeclare but let does not. 
+// Let is also block scoped.
+//- What is the difference between var and const?
+// Const does not let you redeclare
+// Const does not create a hoisted variable, it is block scoped. 
+//- What is the difference between let and const?
+// Let allows you to reassign but you are not able to do with Const.
+//- What is hoisting?
+// Hoisting compiles a variable before it is defined in the scope.
